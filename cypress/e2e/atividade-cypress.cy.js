4d28318d524e867a1b8b1560a5f018ac619b3ded
beatris.sbrigante <beatris_silverio@outlook.com>
2023-08-15 20:23:40 -0300

    alterei o arquivo

--- cypress/e2e/atividade-cypress.cy.js
+++ cypress/e2e/atividade-cypress.cy.js
@@ -1,6 +1,5 @@
 describe('Teste Cypress', () => {
   it('Testar Cadastro', () => {
-
     cy.visit('http://teste-cypress.s3-website-us-east-1.amazonaws.com/')
     cy.wait(1000)
     cy.get('a[href="cadastrar.html"]').click() 
@@ -8,6 +7,8 @@ describe('Teste Cypress', () => {
     cy.get('input[name="user_email"]').type('cypress@teste.com')
     cy.get('input[name="user_password"]').type('cypress')
     cy.get('input[name="user_confirm_password"]').type('cypress')
+    cy.get('button[type="submit"]').click()
+    cy.contains('Você está logado!').should('be.visible')
   })
 
   it('Testar Login', () => {
